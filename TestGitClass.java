c89ae1856f8231575c14dfc861e3cda27f41e0d8
MD SADAQUR RAHMAN <143570510+Sadaqur-github90@users.noreply.github.com>
2023-08-30 01:09:46 -0400

    Update TestGitClass.java

--- TestGitClass.java
+++ TestGitClass.java
@@ -3,5 +3,6 @@ package GitTest;
 public class TestGitClass {
     public static void main(String[] args) {
         System.out.println("Test Git Class success or fail");
+        System.out.println("Add new repository to git hub")
     }
 }
